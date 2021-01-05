f976d2a54f98162570e0b4abfc9ddf175ecca7e7
RenShuaiQi <ren2259402551@163.com>
2021-01-05 19:16:11 +0800

    redis

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,5 +1,10 @@
+import redis.clients.jedis.Jedis;
+
 public class test {
     public static void main(String[] args) {
-        System.out.println("hello,world");
+        Jedis jedis = new Jedis("localhost");
+        //jedis就是指向redis数据库的链接
+        jedis.set("foo","bar");
+        System.out.println(jedis.get("foo"));
     }
 }
